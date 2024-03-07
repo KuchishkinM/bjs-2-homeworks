c89b446ea7b362328206d7c04e0e96b944dc1106
Maksim Kuchishkin <kuchishkin.m@gmail.com>
2024-03-07 12:31:11 +0300

    add objects home work

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.marks ? this.subject = subjectName : console.log('Студент отчислен.Добавление предмета невозможно!!!')
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    this.marks ? this.marks.push(...marks) : console.log('Студент отчислен.Добавление оценок невозможно!!!')
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks && this.marks.length > 0) {
+        return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
+
+let student = new Student("Василиса", "женский", 19);
